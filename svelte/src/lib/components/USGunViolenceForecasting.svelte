97f1efe10d8948b78b89790b19a5a05312de3d30
Charlie Yaris <charlieyaris@gmail.com>
2025-02-26 19:47:24 -0600

    pulling csv.

--- svelte/src/lib/components/USGunViolenceForecasting.svelte
+++ svelte/src/lib/components/USGunViolenceForecasting.svelte
@@ -12,6 +12,31 @@
       svgHeight = (height - graphStrokeSize * 2) * 0.65
     }
   }
+  fetch(
+    "https://raw.githubusercontent.com/cyaris/us_gun_violence_forecasting/refs/heads/master/Web%20Interface/assets/csv/us_gun_violence_forecasting/us_harmed_victim_forecast_data.csv"
+  )
+    .then(response => response.text())
+    .then(csvText => {
+      let rows = csvText.trim().split("\n")
+      let headers = rows.shift().split(",")
+
+      let json = rows.map(row => {
+        let values = row.split(",")
+        return headers.reduce((acc, header, i) => {
+          acc[header] =
+            header == "date"
+              ? Date(values[i])
+              : ["num_harmed", "year"].includes(header)
+                ? parseInt(values[i])
+                : parseFloat(values[i])
+          acc.index = i
+          return acc
+        }, {})
+      })
+
+      console.log(json)
+    })
+    .catch(error => console.error("Error fetching CSV:", error))
 </script>
 
 <div class="flex flex-col w-full h-screen items-center" bind:clientWidth={width} bind:clientHeight={height}>
