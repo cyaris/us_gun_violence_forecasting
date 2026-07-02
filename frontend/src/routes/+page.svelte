314a4961ad815064f202dc207dd1e40262245b19
Charlie Yaris <charlieyaris@charlies-mbp.mynetworksettings.com>
2026-07-01 20:50:14 -0500

    Match root route to app navigation

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
-  import Tool from "./tool/+page.svelte"
+  import { Button } from "svelte-lib/components"
 </script>
 
-<Tool />
+<div class="mb-8 flex flex-col items-center">
+  <Button classes="w-[222.5px]" href="tool" label="Start Forecasting" />
+  <div class="mt-2 grid grid-cols-1 gap-x-1 gap-y-2 sm:grid-cols-2">
+    <Button classes="w-[222.5px]" href="development" label="Development" />
+    <Button classes="w-[222.5px]" href="documentation" label="Documentation" />
+  </div>
+</div>
