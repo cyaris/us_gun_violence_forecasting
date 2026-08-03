d505a340f29f31a454ab9327490a66c04fdd5d3c
Charlie Yaris <35279417+cyaris@users.noreply.github.com>
2026-08-02 20:26:26 -0500

    Use renamed shared link button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { Button } from "svelte-lib/components"
+  import { LinkButton } from "svelte-lib/components"
 </script>
 
 <div class="mb-8 flex flex-col items-center">
-  <Button classes="w-[222.5px]" href="tool" label="Start Forecasting" />
+  <LinkButton classes="w-[222.5px]" href="tool" label="Start Forecasting" />
   <div class="mt-2 grid grid-cols-1 gap-x-1 gap-y-2 sm:grid-cols-2">
-    <Button classes="w-[222.5px]" href="development" label="Development" />
-    <Button classes="w-[222.5px]" href="documentation" label="Documentation" />
+    <LinkButton classes="w-[222.5px]" href="development" label="Development" />
+    <LinkButton classes="w-[222.5px]" href="documentation" label="Documentation" />
   </div>
 </div>
